423e76e61a78e3a63814b24ecbc0e7a9e5182ac4
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-05-31 01:09:36 +0200

    :recycle: Refactor SectionSeparator to allow easier dynamic CSS
    Refactors SectionSeparator to be defined on 'page' level instead of
    inside individual components.

--- src/components/Headers/MainHeader/MainHeader.js
+++ src/components/Headers/MainHeader/MainHeader.js
@@ -1,13 +1,10 @@
 import React from "react";
 import styles from "./MainHeader.module.css";
-import SectionSeparator from "../../SectionSeperator/SectionSeparator";
 
 const MainHeader = () => {
   return (
     <header className={styles.header}>
-      <SectionSeparator type="curved" style={styles["section-separator"]}>
-        <h1 className={styles["header__title"]}>Soysambu Conservancy</h1>
-      </SectionSeparator>
+      <h1 className={styles["header__title"]}>Soysambu Conservancy</h1>
     </header>
   );
 };
--- src/container/Homepage/Homepage.js
+++ src/container/Homepage/Homepage.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from "react";
+import SectionSeparator from "../../components/SectionSeperator/SectionSeparator";
 import MainHeader from "../../components/Headers/MainHeader/MainHeader";
+import BlogItems from "../../components/BlogItems/BlogItems";
+import styles from "./Homepage.module.css";
 
 function Homepage() {
   return (
     <Fragment>
-      <MainHeader />
-      {/* Announcements */}
-      {/* Whats new */}
-      {/* NavBar */}
+      <SectionSeparator type="curved" style={styles["section-separator"]}>
+        <MainHeader />
+      </SectionSeparator>
+
+      <SectionSeparator type="semi-circle" style={styles["section-separator"]}></SectionSeparator>
+
+      <SectionSeparator type="square" style={styles["section-separator"]}></SectionSeparator>
     </Fragment>
   );
 }
