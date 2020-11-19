0f287dfc080e4e3986ffb39a87397261aa9f9ab2
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-11-19 02:30:03 +0100

    :construction: Add routing scaffolding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 
 const App = (props) => {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <Route path="/" exact component={Homepage} />
+      </Fragment>
+    </BrowserRouter>
+  );
 };
 
 export default App;
