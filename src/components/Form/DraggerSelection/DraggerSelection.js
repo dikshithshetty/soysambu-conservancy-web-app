449d59b9403aaaaaa41031a3eddc218fb8c63681
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-05 01:59:03 +0100

    :recycle: Refactor DraggerSelection into functional component

--- src/components/Form/DraggerSelection/DraggerSelection.js
+++ src/components/Form/DraggerSelection/DraggerSelection.js
@@ -1,41 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { FaCheck } from "react-icons/fa";
 import { Field } from "redux-form";
 import DraggerWrapper from "../../Dragger/DraggerWrapper/DraggerWrapper";
 import styles from "./DraggerSelection.module.scss";
 
-class DraggerSelection extends Component {
+const DraggerSelection = (props) => {
   /*
-    Props:
-  */
-  renderError = ({ error, touched }) => {
+   */
+  const renderError = ({ error, touched }) => {
     if (touched && error) {
       console.log(error);
     }
   };
 
-  renderDraggerInput = ({ input, meta }) => (
+  const renderDraggerInput = ({ input, meta }) => (
     <label className={styles["label"]}>
-      {`${this.props.name}:`}
+      {`${props.name}:`}
       <DraggerWrapper
-        name={this.props.name}
-        items={this.props.items}
+        name={props.name}
+        items={props.items}
         value={{ val: input.value }}
-        elementAction={(item) => {
-          return input.value === item ? <FaCheck /> : "";
-        }}
+        elementAction={(item) => (input.value === item ? <FaCheck /> : "")}
         elementStyling={`${styles["select"]} ${meta.error && meta.touched ? styles["error"] : ""}`}
-        onStaticClick={(event) => {
-          input.onChange(event.value);
-        }}
+        onStaticClick={(event) => input.onChange(event.value)}
       />
-      {this.renderError(meta)}
+      {renderError(meta)}
     </label>
   );
 
-  render() {
-    return <Field name={this.props.name} component={this.renderDraggerInput} />;
-  }
-}
+  return <Field name={props.name} component={renderDraggerInput} />;
+};
 
 export default DraggerSelection;
