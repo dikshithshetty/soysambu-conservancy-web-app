016cfad26f1723d183b7fe4e06f11345958b0bb1
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-05 21:04:18 +0100

    :construction: Add thumbNail prop to BlogList and adjust style

--- src/components/Blog/BlogList/BlogList.js
+++ src/components/Blog/BlogList/BlogList.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from "react";
 import styles from "./BlogList.module.scss";
 
-const BlogList = ({ blogItems }) => {
+const BlogList = ({ blogItems, thumbNail }) => {
   const mapBlogItems = blogItems.map((blog) => {
     return (
       <Fragment>
-        <div className={styles["blog-thumbnail"]} />
-        <div className={styles["blog-item"]}>
-          <h3>Announcement Title</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elis cras...</p>
-          <span>View more</span>
+        <div className={styles["blog-preview"]}>
+          {thumbNail ? <div className={styles["blog-thumbnail"]} /> : ""}
+          <div className={styles["blog-snippet"]}>
+            <h3>Announcement Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elis cras...</p>
+            <span>View more</span>
+          </div>
         </div>
       </Fragment>
     );
