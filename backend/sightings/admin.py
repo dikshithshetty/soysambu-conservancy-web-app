1c95f64b790efb63dab3fc93837931d77782f3ce
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-06 23:57:31 +0100

    :construction: Add admin for sightings back end

--- backend/sightings/admin.py
+++ backend/sightings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Sighting, GiraffeSighting, GiraffeCount
+
+
+class SightingAdmin(admin.ModelAdmin):
+    list_display = ('datetime', 'latitude', 'longitude', 'weather', 'habitat')
+
+
+class GiraffeSightingAdmin(admin.ModelAdmin):
+    list_display = ('datetime', 'count')
+
+
+class GiraffeCountAdmin(admin.ModelAdmin):
+    list_display = ('sighting', 'type')
+
 
 # Register your models here.
+admin.site.register(Sighting, SightingAdmin)
+admin.site.register(GiraffeSighting, GiraffeSightingAdmin)
+admin.site.register(GiraffeCount, GiraffeCountAdmin)
