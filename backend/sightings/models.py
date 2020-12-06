5db9c4f17cd32200750d051d2aeb04556e29e09a
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-06 23:57:23 +0100

    :construction: Add models for sightings back end

--- backend/sightings/models.py
+++ backend/sightings/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Sighting(models.Model):
+    class Weather(models.TextChoices):
+        SUNNY = 'SUNNY', _('Sunny')
+        PARTIALLY_CLOUDY = 'PARTIALLY_CLOUDY', _('Partially Cloudy')
+        CLOUDY = 'CLOUDY', _('Cloudy')
+        RAIN = 'RAIN', _('Rain')
+
+    class Habitat(models.TextChoices):
+        ACACIA_WOODLAND = 'ACACIA_WOODLAND', _('Acacia Woodland')
+        ACACIA_MIX = 'ACACIA_MIX', _('Acacia Mix')
+        PLAINS = 'PLAINS', _('Plains')
+        LAKEFRONT = 'LAKEFRONT', _('Lakefront')
+
+    datetime = models.DateTimeField()
+    latitude = models.DecimalField(max_digits=7, decimal_places=5)  # longitude = -90 to 90
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)   # longitude = -180 to 180
+    weather = models.CharField(choices=Weather.choices,
+                               max_length=len(max([choice[0] for choice in Weather.choices], key=len)))
+    habitat = models.CharField(choices=Habitat.choices,
+                               max_length=len(max([choice[0] for choice in Habitat.choices], key=len)))
+
+
+class GiraffeSighting(Sighting):
+    count = models.PositiveSmallIntegerField()
+
+
+class GiraffeCount(models.Model):
+    class Type(models.TextChoices):
+        MALES = 'MALES', _('Males')
+        FEMALES = 'FEMALES', _('Females')
+        JUVENILES = 'JUVENILES', _('Juveniles')
+        UNIDENTIFIED = 'UNIDENTIFIED', _('Unidentified')
+
+    sighting = models.ForeignKey(GiraffeSighting, on_delete=models.PROTECT)
+    type = models.CharField(choices=Type.choices,
+                            max_length=len(max([choice[0] for choice in Type.choices], key=len)))
+
+    feeding = models.PositiveSmallIntegerField()
+    standing = models.PositiveSmallIntegerField()
+    walking = models.PositiveSmallIntegerField()
+    lying = models.PositiveSmallIntegerField()
+    fighting = models.PositiveSmallIntegerField()
+    scratching = models.PositiveSmallIntegerField()
