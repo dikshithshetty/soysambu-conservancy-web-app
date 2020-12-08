06817f90790bfa3f44fec26b5ac7c69ebb716fdf
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-08 04:50:47 +0100

    :recycle: Refactor MenuButton out of HeroHeader into Homepage

--- frontend/src/components/Headers/HeroHeader/HeroHeader.js
+++ frontend/src/components/Headers/HeroHeader/HeroHeader.js
@@ -1,14 +1,10 @@
 import React from "react";
 import styles from "./HeroHeader.module.scss";
-import MenuButton from "../../Buttons/MenuButton/MenuButton";
 
 const HeroHeader = (props) => {
-  let style = [styles["header"], styles["theme--" + props.theme]];
-
   return (
-    <header className={style.join(" ")}>
+    <header className={`${styles["header"]} ${props.theme ? styles["theme--"+props.theme] : ''}`}>
       <h1 className={styles["hero-title"]}>{props.children}</h1>
-      <MenuButton />
     </header>
   );
 };
--- frontend/src/pages/Homepage/Homepage.js
+++ frontend/src/pages/Homepage/Homepage.js
@@ -2,14 +2,20 @@ import React from "react";
 import SectionSeparator from "../../components/SectionSeperator/SectionSeparator";
 import HeroHeader from "../../components/Headers/HeroHeader/HeroHeader";
 import BlogList from "../../components/Blog/BlogList/BlogList";
+import MenuButton from "../../components/Buttons/MenuButton/MenuButton";
 import NavBar from "../../components/Navigation/NavBar/NavBar";
 import styles from "./Homepage.module.scss";
 
 const Homepage = () => {
   return (
     <div className={styles["hero-page"]}>
+      {/* Menu Button */}
+      <MenuButton />
+
       {/* Main Header */}
-      <HeroHeader theme="yellow-dark">Soysambu Conservancy</HeroHeader>
+      <HeroHeader theme="yellow-dark">
+        Soysambu Conservancy
+      </HeroHeader>
 
       {/* Announcements */}
       <main>
