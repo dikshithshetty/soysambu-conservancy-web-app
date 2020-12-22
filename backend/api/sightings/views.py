d50baecfc9ad0eb3d62bbc6f4ca90c528be53732
Niels van der Vegt <ns.vandervegt@gmail.com>
2020-12-23 00:40:22 +0100

    :construction: Add pagination to sightings API responses

--- backend/api/sightings/views.py
+++ backend/api/sightings/views.py
@@ -1,6 +1,13 @@
 from rest_framework import generics, permissions
+from rest_framework.pagination import PageNumberPagination
 from .serializers import SightingListSerializer, GiraffeSightingSerializer
 from .models import Sighting, GiraffeSighting
+
+
+class ResultPaginationMixin(PageNumberPagination):
+    page_size = 5
+    max_page_size = 1000
+    page_size_query_param = 'page_size'
 
 
 class SightingSubClassMixin(object):
@@ -8,12 +15,14 @@ class SightingSubClassMixin(object):
         return Sighting.objects.select_subclasses()
 
 
-class SightingList(SightingSubClassMixin, generics.ListAPIView):
+class SightingList(SightingSubClassMixin, generics.ListAPIView, PageNumberPagination):
     serializer_class = SightingListSerializer
+    pagination_class = ResultPaginationMixin
     # permission_classes = (permissions.IsAuthenticated,)
 
 
 class GiraffeSightingList(generics.ListCreateAPIView):
     serializer_class = GiraffeSightingSerializer
+    pagination_class = ResultPaginationMixin
     # permission_classes = (permissions.IsAuthenticated,)
     queryset = GiraffeSighting.objects.all()
